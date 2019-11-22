d94df68674f021a813374d30ecb3d51ca5437261
prowincial <maximelshumak@gmail.com>
2019-11-22 14:32:25 +0100

    1.0.1

--- app/src/main/java/com/example/portfolio/photos.kt
+++ app/src/main/java/com/example/portfolio/photos.kt
@@ -1,27 +1,41 @@
 package com.example.portfolio
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.github.chrisbanes.photoview.PhotoView
 
 
 class photos : AppCompatActivity()  {
-    private var isImageScaled = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photos)
+        var mIcon: ImageView = findViewById(R.id.ph1)
+        mIcon.setOnClickListener {
+            val mBuilder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this@photos)
+            val mView: View =
+                layoutInflater.inflate(R.layout.dialog_custom_layout, null)
+            val photoView: PhotoView = mView.findViewById(R.id.imageView)
+            photoView.setImageResource(R.drawable.a)
+            mBuilder.setView(mView)
+            val mDialog: android.app.AlertDialog? = mBuilder.create()
+            mDialog?.show()
+        }
+        var mIcon1: ImageView = findViewById(R.id.pho2)
+        mIcon1.setOnClickListener {
+            val mBuilder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this@photos)
+            val mView: View =
+                layoutInflater.inflate(R.layout.dialog_custom_layout, null)
+            val photoView: PhotoView = mView.findViewById(R.id.imageView)
+            photoView.setImageResource(R.drawable.s)
+            mBuilder.setView(mView)
+            val mDialog: android.app.AlertDialog? = mBuilder.create()
+            mDialog?.show()
+        }
     }
-    fun go_photo1 (view: View) {
 
-        startActivity(Intent(this@photos,photo1::class.java))
-
-    }
-    fun go_photo2 (view: View) {
-
-        startActivity(Intent(this@photos,photo2::class.java))
-
-    }
 }

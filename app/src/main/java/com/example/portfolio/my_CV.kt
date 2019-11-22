f06af7ff1ba8e0f963dca49b492723f60cf6b056
prowincial <maximelshumak@gmail.com>
2019-11-22 14:22:50 +0100

    1.0.1

--- app/src/main/java/com/example/portfolio/my_CV.kt
+++ app/src/main/java/com/example/portfolio/my_CV.kt
@@ -1,21 +1,37 @@
 package com.example.portfolio
 
-import androidx.appcompat.app.AppCompatActivity
+
+
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.github.chrisbanes.photoview.PhotoView
+
 
 class my_CV : AppCompatActivity() {
     private var isImageScaled = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my__cv)
 
-    val image_view = findViewById(R.id.mycvii) as ImageView
-    image_view.setOnClickListener { v: View ->
-        if(!isImageScaled) v.animate().scaleX(1.5f).scaleY(1.5f).duration=500
-        if(isImageScaled) v.animate().scaleX(1f).scaleY(1f).duration=500
-        isImageScaled= !isImageScaled
-    }}
+
+        var mIcon: ImageView = findViewById(R.id.mycvii)
+        mIcon.setOnClickListener {
+            val mBuilder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this@my_CV)
+            val mView: View =
+                layoutInflater.inflate(R.layout.dialog_custom_layout, null)
+            val photoView: PhotoView = mView.findViewById(R.id.imageView)
+            photoView.setImageResource(R.drawable.cv)
+            mBuilder.setView(mView)
+            val mDialog: android.app.AlertDialog? = mBuilder.create()
+            mDialog?.show()
+        }
+    }
+
+
+
 }

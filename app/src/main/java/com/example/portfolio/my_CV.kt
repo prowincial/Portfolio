fd9de7d989284139af202c822bcc2fa8bce36bee
prowincial <maximelshumak@gmail.com>
2019-11-22 13:57:38 +0100

    1.0.0

--- app/src/main/java/com/example/portfolio/my_CV.kt
+++ app/src/main/java/com/example/portfolio/my_CV.kt
@@ -2,11 +2,20 @@ package com.example.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class my_CV : AppCompatActivity() {
+    private var isImageScaled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my__cv)
-    }
+
+    val image_view = findViewById(R.id.mycvii) as ImageView
+    image_view.setOnClickListener { v: View ->
+        if(!isImageScaled) v.animate().scaleX(1.5f).scaleY(1.5f).duration=500
+        if(isImageScaled) v.animate().scaleX(1f).scaleY(1f).duration=500
+        isImageScaled= !isImageScaled
+    }}
 }
